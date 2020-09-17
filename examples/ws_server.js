308cef75e70a2a9349505d84729042d5caa33cdd
geekbleek <casey.bleeker@onereach.ai>
2020-09-16 22:48:12 -0600

    Update docs and example WSS protocol from audiostream.drachtio.org to audio.drachtio.org

--- examples/ws_server.js
+++ examples/ws_server.js
@@ -7,28 +7,27 @@ let wstream;
 
 console.log(`listening on port ${port}, writing incoming raw audio to file ${recordingPath}`);
 
-const wss = new WebSocket.Server({ 
-  port,
-  handleProtocols: (protocols, req) => {
-    return 'audiostream.drachtio.org';
-  }
+const wss = new WebSocket.Server({
+    port,
+    handleProtocols: (protocols, req) => {
+        return 'audio.drachtio.org';
+    }
 });
- 
+
 wss.on('connection', (ws, req) => {
-  console.log(`received connection from ${req.connection.remoteAddress}`);
-  wstream = fs.createWriteStream(recordingPath);
+    console.log(`received connection from ${req.connection.remoteAddress}`);
+    wstream = fs.createWriteStream(recordingPath);
 
-  ws.on('message',  (message) => {
-    if (typeof message === 'string') {
-      console.log(`received message: ${message}`);
-    }
-    else if (message instanceof Buffer) {
-      wstream.write(message);
-    }
-  });
+    ws.on('message', (message) => {
+        if (typeof message === 'string') {
+            console.log(`received message: ${message}`);
+        } else if (message instanceof Buffer) {
+            wstream.write(message);
+        }
+    });
 
-  ws.on('close', (code, reason) => {
-    console.log(`socket closed ${code}:${reason}`);
-    wstream.end();
-  });
+    ws.on('close', (code, reason) => {
+        console.log(`socket closed ${code}:${reason}`);
+        wstream.end();
+    });
 });
